8385e1f21ea3ade9f9837db67377d9e385e6424d
YK136185 <YK136185@greendreamteam.onmicrosoft.com>
2025-05-22 13:27:13 +0300

    :rocket: Deploy to ACR

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
 import os
 from dotenv import load_dotenv
-# from azure.storage.blob import BlobServiceClient
+from azure.storage.blob import BlobServiceClient
 
 from db_connections.connection import connect, disconnect
 from utils.logger.write import logger
@@ -11,29 +11,28 @@ load_dotenv()
 
 def main():
     try:
-        logger.info(f"Start")
-
+        logger.info("Start!!!")
         db = connect()
         AZURE_CONNECTION_STRING = os.getenv("AZURE_CONNECTION_STRING")
         CONTAINER_NAME = os.getenv("AZURE_CONTAINER_NAME")
-        logger.info(f"CONTAINER_NAME {CONTAINER_NAME}")
-        logger.info(f"AZURE_CONNECTION_STRING {AZURE_CONNECTION_STRING}")
-        # blob_service_client = BlobServiceClient.from_connection_string(
-        #     AZURE_CONNECTION_STRING
-        # )
-        # container_client = blob_service_client.get_container_client(CONTAINER_NAME)
+        logger.info(f"AZURE_CONNECTION_STRING {AZURE_CONNECTION_STRING}" )
+        logger.info(f"AZURE_CONTAINER_NAME {CONTAINER_NAME}" )
+        blob_service_client = BlobServiceClient.from_connection_string(
+            AZURE_CONNECTION_STRING
+        )
+        container_client = blob_service_client.get_container_client(CONTAINER_NAME)
 
-        # for blob in container_client.list_blobs():
-        #     blob_client = container_client.get_blob_client(blob.name)
-        #     try:
-        #         extract_and_process_blob(blob_client, blob.name, db)
+        for blob in container_client.list_blobs():
+            blob_client = container_client.get_blob_client(blob.name)
+            try:
+                extract_and_process_blob(blob_client, blob.name, db)
 
-        #     except Exception as error:
-        #         error_log = f"Failed to process {blob.name}"
-        #         logger.error(error_log, exc_info=True)
-        #         raise Exception(error_log) from error
+            except Exception as error:
+                error_log = f"Failed to process {blob.name}"
+                logger.error(error_log, exc_info=True)
+                raise Exception(error_log) from error
 
-            # disconnect()
+            disconnect()
 
     except Exception as error:
         error_log = "Failed to process blob"
