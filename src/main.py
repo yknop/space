51ffd8c5d367934ff7d91c7110149327d8ac9333
YK136185 <YK136185@greendreamteam.onmicrosoft.com>
2025-06-19 10:05:38 +0300

    :rocket: Try to deploy

--- src/main.py
+++ src/main.py
@@ -5,34 +5,34 @@ from concurrent.futures import ThreadPoolExecutor as PoolExecutor
 
 from azure.storage.blob import BlobServiceClient
 
-from db_connections.connection import connect, disconnect
-from utils.azure.connect_azure import connect_azure
-from utils.azure.extract_blob import extract_blob
-from utils.env.get_env import get_env
-from utils.images.manage_images import check_image
+# from db_connections.connection import connect, disconnect
+# from utils.azure.connect_azure import connect_azure
+# from utils.azure.extract_blob import extract_blob
+# from utils.env.get_env import get_env
+# from utils.images.manage_images import check_image
 from utils.logger.write import get_logger
 
-env = get_env()
+# env = get_env()
 logger = get_logger()
 
 
-def execute_check_image(blob_name: str) -> str:
-    try:
-        db = connect()
-        blob_client = connect_azure(blob_name)
-        path = extract_blob(blob_client, blob_name)
-        check_image(db, f"{path}")
-        disconnect()
-        shutil.rmtree(os.path.dirname(path))
+# def execute_check_image(blob_name: str) -> str:
+#     try:
+#         db = connect()
+#         blob_client = connect_azure(blob_name)
+#         path = extract_blob(blob_client, blob_name)
+#         check_image(db, f"{path}")
+#         disconnect()
+#         shutil.rmtree(os.path.dirname(path))
 
-        logger.info(f"{blob_name} processed successfully.")
-    except Exception:
-        logger.info(f"{blob_name} failed:\n{traceback.format_exc()}")
+#         logger.info(f"{blob_name} processed successfully.")
+#     except Exception:
+#         logger.info(f"{blob_name} failed:\n{traceback.format_exc()}")
 
 
 def main() -> None:
     try:
-        logger.info("🚀 Starting parallel blob processing........................................................")
+        logger.info("🚀 Starting parallel blob processing.............")
         # num_workers = env.num_workers
         # blob_service_client = BlobServiceClient.from_connection_string(env.azure_connection_string)
         # container_client = blob_service_client.get_container_client(env.azure_container_name)
